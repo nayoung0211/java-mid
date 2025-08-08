00b634464a71e58de86f48ea9becaaee56edf219
nayoung <noela0202@naver.com>
2025-08-08 14:46:49 +0900

    java-mid-25.08.08

--- src/exception/ex4/MainV4.java
+++ src/exception/ex4/MainV4.java
@@ -25,7 +25,7 @@ public class MainV4 {
     }
     //공통 예외 처리
     private static void exceptionHandler(Exception e) {
-//공통 처리
+        //공통 처리
         System.out.println("사용자 메시지: 죄송합니다. 알 수 없는 문제가 발생했습니다.");
         System.out.println("==개발자용 디버깅 메시지==");
         e.printStackTrace(System.out); // 스택 트레이스 출력
